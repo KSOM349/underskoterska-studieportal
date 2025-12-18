df65c25207e75c0859a1f98bee20e1fe8c985834
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-18 15:20:38 +0100

    Add firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, getDocs, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+  GNU nano 7.2                 firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/1>
+import { getFirestore, collection, getDocs, updateDoc, doc } from ">
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJsZ4LZVrBucavpTdhXbKxyE_BFeZFFKs",
@@ -20,5 +21,22 @@ export async function loadKurser() {
 
 export async function saveAktiv(id, aktiv) {
   await updateDoc(doc(db, "kurser", id), { aktiv });
-}
+}// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJsZ4LZVrBucavpTdhXbKxyE_BFeZFFKs",
+  authDomain: "fir-console-df3e9.firebaseapp.com",
+  projectId: "fir-console-df3e9",
+  storageBucket: "fir-console-df3e9.firebasestorage.app",
+  messagingSenderId: "750795336412",
+  appId: "1:750795336412:web:abfd0c06941a9418abe219"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+// اختبار سريع
+console.log("Firebase connected");
 
