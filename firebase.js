a68e8bb6866cb7b3b84498acaba42bb750853819
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-19 04:19:24 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,15 +4,17 @@ import {
   getFirestore,
   collection,
   getDocs,
+  addDoc,
   updateDoc,
   doc
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+// إعداد Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDJsZ4LZVrBucavpTdhXbKxyE_BFeZFFKs",
+  apiKey: "PUT_YOUR_KEY",
   authDomain: "fir-console-df3e9.firebaseapp.com",
   projectId: "fir-console-df3e9",
-  storageBucket: "fir-console-df3e9.firebasestorage.app",
+  storageBucket: "fir-console-df3e9.appspot.com",
   messagingSenderId: "750795336412",
   appId: "1:750795336412:web:abfd0c06941a9418abe219"
 };
@@ -20,14 +22,23 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
+// جلب الكورسات
 export async function loadKurser() {
   const snap = await getDocs(collection(db, "kurser"));
   return snap.docs.map(d => ({ id: d.id, ...d.data() }));
 }
 
+// تغيير حالة الكورس
 export async function saveAktiv(id, aktiv) {
   await updateDoc(doc(db, "kurser", id), { aktiv });
 }
 
-console.log("🔥 Firebase connected");
+// حفظ uppgift
+export async function saveUppgift(data) {
+  await addDoc(collection(db, "uppgifter"), {
+    ...data,
+    createdAt: new Date()
+  });
+}
 
+console.log("Firebase connected");
