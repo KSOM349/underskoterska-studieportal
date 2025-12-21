ca999858dca298806a3c47b22625641854ea508b
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-21 18:10:32 +0100

    FINAL fix: kurser render works

--- kurser.js
+++ kurser.js
@@ -1,10 +1,19 @@
-import { loadKurser, saveAktiv } from "./firebase.js";
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-const container = document.getElementById("kurser-container");
+const container = document.getElementById("kurser");
+
+async function loadKurser() {
+  const snap = await getDocs(collection(db, "kurser"));
+  return snap.docs.map(doc => doc.data());
+}
 
 async function renderKurser() {
-  const kurser = await loadKurser();
+  if (!container) return;
+
   container.innerHTML = "";
+
+  const kurser = await loadKurser();
 
   kurser.forEach(kurs => {
     const div = document.createElement("div");
@@ -12,18 +21,12 @@ async function renderKurser() {
 
     div.innerHTML = `
       <div>
-        <div><strong>${kurs.name}</strong></div>
-        <div class="${kurs.aktiv ? "aktiv" : "ejaktiv"}">
+        <strong>${kurs.name}</strong><br>
+        <span class="${kurs.aktiv ? "aktiv" : "ejaktiv"}">
           ${kurs.aktiv ? "Aktiv" : "Ej aktiv"}
-        </div>
+        </span>
       </div>
-      <button>Ändra</button>
     `;
-
-    div.querySelector("button").onclick = async () => {
-      await saveAktiv(kurs.id, !kurs.aktiv);
-      renderKurser();
-    };
 
     container.appendChild(div);
   });
