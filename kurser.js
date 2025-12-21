bfe2b7a439c8c8ed2df7e8c8e4481574aca1106d
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-21 18:26:13 +0100

    Fix kurser.js

--- kurser.js
+++ kurser.js
@@ -1,35 +1,14 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-const container = document.getElementById("kurser");
+const firebaseConfig = {
+  apiKey: "PUT_YOUR_API_KEY_HERE",
+  authDomain: "fir-console-df3e9.firebaseapp.com",
+  projectId: "fir-console-df3e9",
+  storageBucket: "fir-console-df3e9.appspot.com",
+  messagingSenderId: "PUT_SENDER_ID_HERE",
+  appId: "PUT_APP_ID_HERE"
+};
 
-async function loadKurser() {
-  const snap = await getDocs(collection(db, "kurser"));
-  return snap.docs.map(doc => doc.data());
-}
-
-async function renderKurser() {
-  if (!container) return;
-
-  container.innerHTML = "";
-
-  const kurser = await loadKurser();
-
-  kurser.forEach(kurs => {
-    const div = document.createElement("div");
-    div.className = "kurs";
-
-    div.innerHTML = `
-      <div>
-        <strong>${kurs.name}</strong><br>
-        <span class="${kurs.aktiv ? "aktiv" : "ejaktiv"}">
-          ${kurs.aktiv ? "Aktiv" : "Ej aktiv"}
-        </span>
-      </div>
-    `;
-
-    container.appendChild(div);
-  });
-}
-
-renderKurser();
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
