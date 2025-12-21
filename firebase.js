4652ba3d873f9b75a3cf5ba06074a9e1a1e445d9
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-21 18:30:35 +0100

    Fix Firestore default native

--- firebase.js
+++ firebase.js
@@ -1,38 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  updateDoc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJsZ4LZVrBucavpTdhXbKxyE_BFeZFFKs",
+  apiKey: "API_KEY",
   authDomain: "fir-console-df3e9.firebaseapp.com",
   projectId: "fir-console-df3e9",
   storageBucket: "fir-console-df3e9.appspot.com",
-  messagingSenderId: "750795336412",
-  appId: "1:750795336412:web:abfd0c06941a9418abe219"
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-export async function loadKurser() {
-  const snap = await getDocs(collection(db, "kurser"));
-  return snap.docs.map(d => ({
-    id: d.id,
-    ...d.data()
-  }));
-}
-
-export async function saveAktiv(id, aktiv) {
-  const ref = doc(db, "kurser", id);
-  await updateDoc(ref, { aktiv });
-}
-
-export async function saveGroup(name, data) {
-  await setDoc(doc(db, "groups", name), data);
-}
