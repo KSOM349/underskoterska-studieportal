8a5c6ef69b26e74ffd274a87a8649beebe26e000
Mugrani Djerf <sksomugrani07@gmail.com>
2025-12-22 09:32:16 +0100

    Fix: correct Firebase CDN config for Firestore

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PUT_API_KEY_HERE",
+  apiKey: "AIzaSyDJsZ4LZVrBucavpTdhXbKxyE_BFeZFFKs",
   authDomain: "fir-console-df3e9.firebaseapp.com",
   projectId: "fir-console-df3e9",
-  storageBucket: "fir-console-df3e9.appspot.com",
-  messagingSenderId: "PUT_SENDER_ID_HERE",
-  appId: "PUT_APP_ID_HERE"
+  storageBucket: "fir-console-df3e9.firebasestorage.app",
+  messagingSenderId: "750795336412",
+  appId: "1:750795336412:web:abfd0c06941a9418abe219"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
